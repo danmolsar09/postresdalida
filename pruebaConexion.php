7320be23601f253477533213ad5a38b773c23740
jesszf <jesszf@users.noreply.github.com>
2020-03-28 23:08:46 -0600

    codigo de conexión a la base de datos
    conexión exitosa

--- pruebaConexion.php
+++ pruebaConexion.php
@@ -1,3 +1,18 @@
-<? php
+<?php
+	
+	$host = 'localhost';
+	$user = 'root';
+	$password = '';
+	$db = 'reposteria_dalida';
+
+	$conection = @mysqli_connect ($host, $user, $password, $db);
+
+	if (!$conection) {
+
+	echo "Error en la conexión";
+
+	}else {
+	echo "conexión exitosa" ;
+	}
 
 ?>
